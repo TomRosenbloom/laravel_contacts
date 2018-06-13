4383600bc364efb1184c6a88c5727f8e7606b562
TomRosenbloom <TomRosenbloom@users.noreply.github.com>
2018-06-13 14:36:43 +0100

    added algolia search components

--- app/Contact.php
+++ app/Contact.php
@@ -2,17 +2,37 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
     use SoftDeletes;
+    use Searchable;
+
     protected $dates = ['deleted_at'];
 
     public function addresses()
     {
         return $this->belongsToMany('App\Address', 'address_contact')->withPivot('is_default','address_type_id');
+    }
+
+    /**
+     * overide Scout method toSearchableArray to select specific cols to be added to search index
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        return [
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+
+            'email' => $this->email,
+            'telephone' => $this->telephone,
+            'postcode' => $this->postcode,
+        ];
     }
 
 }
